7629b6dde8dbfbaddc3d2c67bfa3d71f5f60a8b8
yagomotti <yagomotti@gmail.com>
2021-04-07 21:53:04 -0300

    Servidor e diretórios

---
+++ ChatServidor/frmServidor.Designer.cs
@@ -0,0 +1,96 @@
+﻿
+namespace ChatServidor
+{
+    partial class frmServidor
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtIP = new System.Windows.Forms.TextBox();
+            this.btnAtender = new System.Windows.Forms.Button();
+            this.txtLog = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(39, 41);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(75, 15);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Endereço IP: ";
+            // 
+            // txtIP
+            // 
+            this.txtIP.Location = new System.Drawing.Point(120, 38);
+            this.txtIP.Name = "txtIP";
+            this.txtIP.Size = new System.Drawing.Size(222, 23);
+            this.txtIP.TabIndex = 1;
+            // 
+            // btnAtender
+            // 
+            this.btnAtender.Location = new System.Drawing.Point(348, 41);
+            this.btnAtender.Name = "btnAtender";
+            this.btnAtender.Size = new System.Drawing.Size(121, 23);
+            this.btnAtender.TabIndex = 2;
+            this.btnAtender.Text = "Iniciar Atendimento";
+            this.btnAtender.UseVisualStyleBackColor = true;
+            this.btnAtender.Click += new System.EventHandler(this.btnAtender_Click);
+            // 
+            // txtLog
+            // 
+            this.txtLog.Location = new System.Drawing.Point(120, 76);
+            this.txtLog.Multiline = true;
+            this.txtLog.Name = "txtLog";
+            this.txtLog.Size = new System.Drawing.Size(303, 274);
+            this.txtLog.TabIndex = 3;
+            // 
+            // frmServidor
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(495, 409);
+            this.Controls.Add(this.txtLog);
+            this.Controls.Add(this.btnAtender);
+            this.Controls.Add(this.txtIP);
+            this.Controls.Add(this.label1);
+            this.Name = "frmServidor";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtIP;
+        private System.Windows.Forms.Button btnAtender;
+        private System.Windows.Forms.TextBox txtLog;
+    }
+}
+
---
+++ ChatServidor/frmServidor.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+
+namespace ChatServidor
+{
+    public partial class frmServidor : Form
+    {
+        public frmServidor()
+        {
+            InitializeComponent();
+        }
+
+        private delegate void AtualizaStatusCallback(string strMensagem);
+
+        public void mainServidor_StatusChanged(object sender, StatusChangedEventArgs e)
+        {
+            // Chama o método que atualiza o formulário
+            this.Invoke(new AtualizaStatusCallback(this.AtualizaStatus), new object[] { e.EventMessage });
+        }
+
+        private void AtualizaStatus(string strMensagem)
+        {
+            // Atualiza o logo com mensagens
+            txtLog.AppendText(strMensagem + "\r\n");
+        }
+
+
+        private void btnAtender_Click(object sender, EventArgs e)
+        {
+            // Analisa o endereço IP do servidor informado no textbox
+            IPAddress enderecoIP = IPAddress.Parse(txtIP.Text);
+
+            // Cria uma nova instância do objeto ChatServidor
+            ChatServidor mainServidor = new ChatServidor(enderecoIP);
+
+            // Vincula o tratamento de evento StatusChanged a mainServer_StatusChanged
+            ChatServidor.StatusChanged += new StatusChangedEventHandler(mainServidor_StatusChanged);
+
+            // Inicia o atendimento das conexões
+            mainServidor.IniciaAtendimento();
+
+            // Mostra que nos iniciamos o atendimento para conexões
+            txtLog.AppendText("Monitorando as conexões...\r\n");
+        }
+    }
+}
